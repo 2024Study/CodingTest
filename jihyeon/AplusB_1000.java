8903da4bda63174001e51c34b4ffee8aa8d45e02
kim jihyeon <jihyeon546@gmail.com>
2024-12-16 23:58:52 +0900

    [solve]: 1000_sprout

---
+++ jihyeon/AplusB_1000.java
@@ -0,0 +1,24 @@
+package CodingTest.jihyeon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class AplusB_1000 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String input = br.readLine();
+        StringTokenizer st = new StringTokenizer(input);
+
+        int a, b = 0;
+        try {
+            a = Integer.parseInt(st.nextToken());
+            b = Integer.parseInt(st.nextToken());
+        } catch (NumberFormatException e) {
+            throw new NumberFormatException();
+        }
+        int sum = a + b;
+        System.out.println(sum);
+    }
+}
