41c4c930bb334d2b6db7a9d4f5399a4fd3460536
soapcookie <sgua0720@gmail.com>
2024-12-21 22:16:05 +0900

    [review]:배열로 변경

--- sua/VerificationNumber_2475.java
+++ sua/VerificationNumber_2475.java
@@ -12,13 +12,25 @@ public class VerificationNumber_2475 {
         StringTokenizer st = new StringTokenizer(br.readLine());
 
         //for문으로 다시.
-        int A = Integer.parseInt(st.nextToken());
-        int B = Integer.parseInt(st.nextToken());
-        int C = Integer.parseInt(st.nextToken());
-        int D = Integer.parseInt(st.nextToken());
-        int E = Integer.parseInt(st.nextToken());
+//        int A = Integer.parseInt(st.nextToken());
+//        int B = Integer.parseInt(st.nextToken());
+//        int C = Integer.parseInt(st.nextToken());
+//        int D = Integer.parseInt(st.nextToken());
+//        int E = Integer.parseInt(st.nextToken());
+//
+//        int res = (A*A + B*B + C*C + D*D + E*E)%10;
 
-        int res = (A*A + B*B + C*C + D*D + E*E)%10;
+        int [] numbers = new int[5];
+
+        int sum = 0;
+
+        for (int i=0;i<5;i++){
+            numbers[i] = Integer.parseInt(st.nextToken());
+            sum +=numbers[i]*numbers[i];
+        }
+
+        int res = sum % 10;
+
 
         bw.write(String.valueOf(res));
         bw.flush();
