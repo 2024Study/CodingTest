c2386c4b428f2cda6c4eb8a3aa4fb4057dfe90c5
kim jihyeon <jihyeon546@gmail.com>
2024-12-21 20:59:11 +0900

    [solve]: 2457_sprout

---
+++ jihyeon/ValidateNumber_2457.java
@@ -0,0 +1,28 @@
+package CodingTest.jihyeon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class ValidateNumber_2457 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int[] numbers = new int[5];
+        int sum = 0;
+        int result;
+
+        for (int i = 0; i < 5; i++) {
+            try {
+                numbers[i] = Integer.parseInt(st.nextToken());
+            } catch (NumberFormatException e) {
+                throw new NumberFormatException(e.getMessage());
+            }
+            sum = sum + numbers[i]*numbers[i];
+        }
+        result = sum % 10;
+
+        System.out.println(result);
+    }
+}
