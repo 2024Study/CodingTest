7459b5c9a9cbf62fe09ff107eb5f2719ce343ab0
soapcookie <sgua0720@gmail.com>
2024-12-23 18:53:53 +0900

    [solve]:27866_sprout

--- sua/CharacterAndStrings_27866.java
+++ sua/CharacterAndStrings_27866.java
@@ -7,12 +7,19 @@ public class CharacterAndStrings_27866 {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-        String stringInput = br.readLine();
+        //배열로 문자열 입력받기
+        //입력받은 정수에 해당하는 배열 문자 반환
 
-        int intInput = Integer.parseInt(br.readLine());
+        char[] arr = br.readLine().toCharArray();
+        int index = Integer.parseInt(br.readLine())-1;
+        char result = arr[index];
 
+        bw.write(result);
+        bw.newLine();
+        bw.flush();
 
-
+        br.close();
+        bw.close();
 
     }
 }
