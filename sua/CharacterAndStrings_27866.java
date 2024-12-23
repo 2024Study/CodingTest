b0c9ca0e589b0303d056558e0e04caf5bfb3d8cb
soapcookie <sgua0720@gmail.com>
2024-12-23 18:39:43 +0900

    [atempt]:27866_sprout

---
+++ sua/CharacterAndStrings_27866.java
@@ -0,0 +1,19 @@
+package CodingTest.sua;
+
+import java.io.*;
+
+public class CharacterAndStrings_27866 {
+    public static void main(String[] args)throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        String stringInput = br.readLine();
+
+        int intInput = Integer.parseInt(br.readLine());
+
+
+
+
+    }
+}
+}
