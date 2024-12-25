24d67fb2d5c2f9c050dd41be5749cb4c816bde0c
soapcookie <sgua0720@gmail.com>
2024-12-25 22:16:46 +0900

    [attempt]:StringRepeat_bronze

---
+++ sua/Bronze/StringRepeat_2675.java
@@ -0,0 +1,24 @@
+package CodingTest.sua.Bronze;
+
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class StringRepeat_2675 {
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int T = Integer.parseInt(br.readLine());
+
+        String res = "";
+
+        for(int i=1; i<=T; i++){
+
+        }
+
+
+    }
+}
