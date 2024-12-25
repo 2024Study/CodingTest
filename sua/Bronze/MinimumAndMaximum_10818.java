dcc1c5833deb8a2799d0e3d78b0062b7940d0f9c
soapcookie <sgua0720@gmail.com>
2024-12-25 11:01:36 +0900

    [attempt]:MinmunAndMaximum_bronze

---
+++ sua/Bronze/MinimumAndMaximum_10818.java
@@ -0,0 +1,27 @@
+package CodingTest.sua.Bronze;
+
+import java.io.*;
+import java.util.Collections;
+import java.util.StringTokenizer;
+
+public class MinimumAndMaximum_10818 {
+
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        StringBuilder sb = new StringBuilder();
+
+        int T = Integer.parseInt(br.readLine());
+
+        for(int i=1; i<=T; i++) {
+
+            StringTokenizer st = new StringTokenizer(br.readLine());
+
+            int num = Integer.parseInt(st.nextToken());
+        }
+
+
+    }
+
+}
