14997627154e01e75b0de17044850db2aa213ef7
user.email <sgua0720@gmail.com>
2024-12-31 23:15:02 +0900

    [attempt]:RightTryangle_bronze

---
+++ sua/Bronze/RightTryangle_4153.java
@@ -0,0 +1,16 @@
+package CodingTest.sua.Bronze;
+
+import java.io.*;
+
+public class RightTryangle_4153 {
+
+    public static void main(String args[]) throws IOException{
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        //각 입력에 대해 직각 삼각형이 맞다면 "right", 아니라면 "wrong"을 출력한다.
+
+    }
+
+}
