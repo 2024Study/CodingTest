2e4c58bc58af94ee49f80005cfb7de76ff52298d
user.email <sgua0720@gmail.com>
2025-01-02 21:17:25 +0900

    [solve]:RightTryangle_bronze

--- sua/Bronze/RightTryangle_4153.java
+++ sua/Bronze/RightTryangle_4153.java
@@ -1,16 +1,36 @@
-package CodingTest.sua.Bronze;
-
 import java.io.*;
+import java.util.*;
 
 public class RightTryangle_4153 {
 
-    public static void main(String args[]) throws IOException{
+    public static void main(String args[]) throws IOException {
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-        //각 입력에 대해 직각 삼각형이 맞다면 "right", 아니라면 "wrong"을 출력한다.
+        while (true) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            int c = Integer.parseInt(st.nextToken());
 
+
+            if (a == 0 && b == 0 && c == 0) break;
+
+
+            int[] squares = {a*a, b*b, c*c};
+            Arrays.sort(squares);  // 오름차순 정렬
+
+
+            if (squares[0] + squares[1] == squares[2]) {
+                bw.write("right\n");
+            } else {
+                bw.write("wrong\n");
+            }
+        }
+
+        bw.flush();
+        bw.close();
+        br.close();
     }
-
 }
