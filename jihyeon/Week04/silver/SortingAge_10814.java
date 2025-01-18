c98b60eb47435b8c49428ba7780a8376d24fc7be
kim jihyeon <jihyeon546@gmail.com>
2025-01-18 23:50:38 +0900

    [attempt]: 10814_silver

---
+++ jihyeon/Week04/silver/SortingAge_10814.java
@@ -0,0 +1,23 @@
+package CodingTest.jihyeon.Week04.silver;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.Dictionary;
+import java.util.Hashtable;
+
+public class SortingAge_10814 {
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int count = Integer.parseInt(br.readLine());
+        Dictionary<Integer, String> users = new Hashtable<Integer, String>();
+
+        for (int i = 0; i < count; i++) {
+
+        }
+    }
+}
