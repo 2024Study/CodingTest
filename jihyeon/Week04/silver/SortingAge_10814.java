e496e4b5a530b42ee0997abfc55c0be9626a4ae9
kim jihyeon <jihyeon546@gmail.com>
2025-01-20 21:35:17 +0900

    [solved]: 10814_silver

--- jihyeon/Week04/silver/SortingAge_10814.java
+++ jihyeon/Week04/silver/SortingAge_10814.java
@@ -5,19 +5,60 @@ import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
+import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.Dictionary;
 import java.util.Hashtable;
+import java.util.List;
 
 public class SortingAge_10814 {
     public static void main(String[] args) throws IOException {
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
         int count = Integer.parseInt(br.readLine());
-        Dictionary<Integer, String> users = new Hashtable<Integer, String>();
+        List<Member> members = new ArrayList<>();
 
         for (int i = 0; i < count; i++) {
-
+            String[] input = br.readLine().split(" ");
+            int age = Integer.parseInt(input[0]);
+            String name = input[1];
+            members.add(new Member(age, name, i)); // 입력 순서를 저장
         }
+
+        members.sort(Comparator.comparingInt(Member::getAge).thenComparingInt(Member::getIndex));
+
+        for (Member member : members) {
+            bw.write(member.getAge() + " " + member.getName() + "\n");
+        }
+
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+}
+
+// 멤버 클래스 정의
+class Member {
+    private final int age;
+    private final String name;
+    private final int index;
+
+    public Member(int age, String name, int index) {
+        this.age = age;
+        this.name = name;
+        this.index = index;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getIndex() {
+        return index;
     }
 }
