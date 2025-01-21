97013c3ac0049281b0e59369b74ef1194304a340
user.email <sgua0720@gmail.com>
2025-01-21 20:33:01 +0900

    [solve] : President_Bronze

--- sua/Bronze/President_2775.java
+++ sua/Bronze/President_2775.java
@@ -1,36 +1,45 @@
 package CodingTest.sua.Bronze;
 
+
 import java.io.*;
 
 public class President_2775 {
     public static void main(String[] args) throws IOException {
-
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
         int T = Integer.parseInt(br.readLine());
 
-        int k = Integer.parseInt(br.readLine());
+        for (int i = 0; i < T; i++) {
+            int k = Integer.parseInt(br.readLine());
+            int n = Integer.parseInt(br.readLine());
 
-        int n = Integer.parseInt(br.readLine());
+            int people = calPeople(k, n);
 
-        int peoeple = calPeople(T, k,n);
+            bw.write(String.valueOf(people));
+            bw.newLine();
+        }
 
-        bw.write(String.valueOf(peoeple));
         bw.flush();
         bw.close();
-
+        br.close();
     }
 
-    private static int calPeople(int T, int k, int n){
+    private static int calPeople(int k, int n) {
+        int[][] apt = new int[k + 1][n + 1];
 
-        /*
-        2층의 3호
-= 1층의 1호 + 1층의 2호 + 1층의 3호
-= (0층의 1호) + (0층의 1호+0층의 2호) + (0층의 1호+0층의 2호+3호)
-= 1 + (1+2)+ (1+2+3)
-=10*/
-        return 0;
+        // 0층 초기화
+        for (int i = 1; i <= n; i++) {
+            apt[0][i] = i;
+        }
+
+        // 1층부터 k층까지 계산
+        for (int i = 1; i <= k; i++) {
+            for (int j = 1; j <= n; j++) {
+                apt[i][j] = apt[i][j - 1] + apt[i - 1][j];
+            }
+        }
+
+        return apt[k][n];
     }
-
 }
