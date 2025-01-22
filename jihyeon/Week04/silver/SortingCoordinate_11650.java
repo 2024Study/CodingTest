41aaedcc040f273e584a57fcf1820474f1fdba90
kim jihyeon <jihyeon546@gmail.com>
2025-01-22 23:34:25 +0900

    [attempt]: 11650_Silver

--- jihyeon/Week04/silver/SortingCoordinate_11650.java
+++ jihyeon/Week04/silver/SortingCoordinate_11650.java
@@ -5,11 +5,21 @@ import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
 
 public class SortingCoordinate_11650 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int N = Integer.parseInt(br.readLine());
+        int[][] coordinates = new int[N][2];
+
+        for (int i = 0; i < N; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            coordinates[i][0] = Integer.parseInt(st.nextToken());
+            coordinates[i][1] = Integer.parseInt(st.nextToken());
+        }
 
     }
 }
