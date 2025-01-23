c27c04b38ec73351b91c5388736a98ee114f94f6
kim jihyeon <jihyeon546@gmail.com>
2025-01-23 22:07:46 +0900

    [solved]: 11650_Silver

--- jihyeon/Week04/silver/SortingCoordinate_11650.java
+++ jihyeon/Week04/silver/SortingCoordinate_11650.java
@@ -5,6 +5,7 @@ import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
+import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class SortingCoordinate_11650 {
@@ -20,6 +21,21 @@ public class SortingCoordinate_11650 {
             coordinates[i][0] = Integer.parseInt(st.nextToken());
             coordinates[i][1] = Integer.parseInt(st.nextToken());
         }
+        Arrays.sort(coordinates, (e1, e2) -> {
+            if (e1[0] == e2[0]) {
+                return e1[1] - e2[1];
+            } else {
+                return e1[0] - e2[0];
+            }
+        });
+
+        for (int i = 0; i < N; i++) {
+            bw.write(coordinates[i][0] + " " + coordinates[i][1] + "\n");
+        }
+
+        bw.flush();
+        bw.close();
+        br.close();
 
     }
 }
