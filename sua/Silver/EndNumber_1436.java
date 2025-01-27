bf048332b920d8c9b09486c63465a0749aea5da5
user.email <sgua0720@gmail.com>
2025-01-27 23:07:56 +0900

    [attempt] : EndNumber_Silver

---
+++ sua/Silver/EndNumber_1436.java
@@ -0,0 +1,21 @@
+package CodingTest.sua.Silver;
+
+import java.io.*;
+
+
+public class EndNumber_1436 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+    }
+
+    private static int calEndNumber(int N){
+        return 0;
+    }
+
+
+
+}
