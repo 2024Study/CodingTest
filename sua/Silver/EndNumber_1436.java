2af3dbf6f399ef9b7b62db8ed43071cd6c844e13
user.email <sgua0720@gmail.com>
2025-01-28 12:39:24 +0900

    [solve] : EndNumber_Silver

--- sua/Silver/EndNumber_1436.java
+++ sua/Silver/EndNumber_1436.java
@@ -10,12 +10,30 @@ public class EndNumber_1436 {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
         int N = Integer.parseInt(br.readLine());
+        int res = calEndNumber(N);
+
+        bw.write(String.valueOf(res));
+        bw.flush();
+        bw.close();
     }
 
     private static int calEndNumber(int N){
-        return 0;
-    }
+        //666부터 시작해서 N번째로 666이 들어가는 숫자 반환
+        // 1~6 까지는 5666, 7~ 6660, 6661
+        int cnt = 0;
+        int num = 666;
 
+        while (cnt <N){
+            if(String.valueOf(num).contains("666")){
+                cnt++;
+            }
+            if (cnt == N){
+                break;
+            }
+            num++;
+        }
+        return num;
+    }
 
 
 }
