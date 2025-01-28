0ea68893853d983f0665063270f6f3f65098ce86
kim jihyeon <jihyeon546@gmail.com>
2025-01-28 22:47:15 +0900

    [attempt]: 1181_silver

--- jihyeon/Week05/silver/SortingWords_1181.java
+++ jihyeon/Week05/silver/SortingWords_1181.java
@@ -5,10 +5,24 @@ import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
 
 public class SortingWords_1181 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int N = Integer.parseInt(br.readLine());
+
+        HashSet<String> set = new HashSet<>();
+
+        // 입력 받기 및 중복 제거
+        for (int i = 0; i < N; i++) {
+            set.add(br.readLine());
+        }
+
+        List<String> list = new ArrayList<>(set);
     }
 }
