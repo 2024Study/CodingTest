a2fa132a063c0c6e8e1fb05838b0fa58285f28a7
user.email <sgua0720@gmail.com>
2025-01-29 15:57:41 +0900

    [attempt] : SortByAge_Silver

---
+++ sua/Silver/SortByAge_10814.java
@@ -0,0 +1,20 @@
+package CodingTest.sua.Silver;
+
+import java.io.*;
+
+public class SortByAge_10814 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+
+
+        /* 1. N의 개수에 따라 나이와 이름을 공백을 기준으로 입력받는 배열을 반복적으로 입력받기
+        *  2. 나이 오름차순 정렬, 나이가 같을 경우에는 먼저 입력된 이름순으로 */
+
+    }
+
+
+}
