5e3e1bddf2e75c59eed4cf6de2b8328850d8269b
user.email <sgua0720@gmail.com>
2025-01-30 23:09:20 +0900

    [attempt] : Bracket_Silver

---
+++ sua/Silver/Bracket_9012.java
@@ -0,0 +1,17 @@
+package CodingTest.sua.Silver;
+
+import java.io.*;
+
+public class Bracket_9012 {
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int T = Integer.parseInt(br.readLine());
+        //괄호짝이 잘맞는지 여부로 NO, YES 출력
+
+        String[] brackets = new String[T];
+
+
+    }
+}
